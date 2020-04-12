955c493570237f16fe6825b76ecc8660dcff7442
uppuluriaditya <uppuluriaditya@gmail.com>
2020-04-12 20:03:35 +0530

    Matching braces

---
+++ chapters/chapter6/matching_brackets.py
@@ -0,0 +1,29 @@
+from stack import ArrayStack
+
+def matching_brackets(expr):
+    open_braces = '({['
+    closed_braces = ')}]'
+
+    S = ArrayStack(1024)
+    for char in expr:
+        if char in open_braces:
+            S.push(char)
+        elif char in closed_braces:
+            if S.is_empty():
+                return False
+            if closed_braces.index(char) != open_braces.index(S.pop()):
+                return False
+    return S.is_empty()
+
+if __name__ == "__main__":
+    print(matching_brackets('()(()){([()])}'))
+
+    print(matching_brackets('((()(()){([()])}))'))
+
+    print(matching_brackets(')(()){([()])}'))
+
+    print(matching_brackets('({[])}'))
+
+
+
+            
